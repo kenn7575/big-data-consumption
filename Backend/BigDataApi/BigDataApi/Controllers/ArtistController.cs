8af6465f32763819a38b2b3cee62456de34d3bfb
Casp103a <casp103a@edu.sde.dk>
2025-03-13 09:00:42 +0100

    Removed api from artistcontroller and made so when you look for artist it ignors the casing.

--- Backend/BigDataApi/BigDataApi/Controllers/ArtistController.cs
+++ Backend/BigDataApi/BigDataApi/Controllers/ArtistController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BigDataApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ArtistsController : ControllerBase
     {
